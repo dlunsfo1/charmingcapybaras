97885be3c549b07fff0f521dd2a97bf1c1c066e3
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-24 08:20:36 -0500

    added in config file

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  user: '',
+  // if you are using the pairing stations, the user is `student`
+  password: '',
+  // if you are using the paring stations, the password is `student`
+  // if you do not have a password, delete this key/value pair from
+  // the object
+  database: '',
+  mail_user: 'rpt05capybaras@gmail.com',
+  mail_pwd: 'hackreactor',
+  google_places_api: 'AIzaSyDAhznStI-GlQl73G3mQYVL9vltK1kw28g',
+  google_geocode_api: 'AIzaSyAyE2zfaHCl8HcZadF2m3JK00zX5bLU2P4'
+};
