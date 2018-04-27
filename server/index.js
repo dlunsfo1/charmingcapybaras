3d7415573c25fd3d48418dd7e4e3cf6548c8ba51
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-27 12:30:33 -0500

    Added access origin to express

--- server/index.js
+++ server/index.js
@@ -32,6 +32,15 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // serve static files from ../client/dist
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
