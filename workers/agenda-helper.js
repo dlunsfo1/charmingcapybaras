da2bc9eedecb419a001e59d669c5b0e737c6c12a
Daren Lunsford <dlunsfo1@asu.edu>
2018-04-27 08:19:03 -0500

    Clean up code

--- workers/agenda-helper.js
+++ workers/agenda-helper.js
@@ -2,7 +2,7 @@ var archive = require('../helpers/utils');
 var cron = require('cron');
 
 var CronJob = require('cron').CronJob;
-
+//  '*/1 * * * *',
 new CronJob(
   '*/1 * * * *',
   function() {
@@ -13,6 +13,3 @@ new CronJob(
   true,
   'America/Los_Angeles'
 );
-
-// worker reads urls in the list
-// worker download files
